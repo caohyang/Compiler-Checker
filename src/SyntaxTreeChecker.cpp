577f8e88150ee0a108af54cfbab9f8b2084bcc87
caohyang <caoyiyang@mail.ustc.edu.cn>
2021-11-12 21:39:48 +0800

    ok

--- src/SyntaxTreeChecker.cpp
+++ src/SyntaxTreeChecker.cpp
@@ -8,7 +8,10 @@ void SyntaxTreeChecker::visit(Assembly& node) {
     }
 }
 
-void SyntaxTreeChecker::visit(FuncDef& node) {}
+void SyntaxTreeChecker::visit(FuncDef& node) {
+    node.body->accept(*this);
+    this->exit_scope();
+}
 
 void SyntaxTreeChecker::visit(BinaryExpr& node) {
     node.lhs->accept(*this);
@@ -28,7 +31,13 @@ void SyntaxTreeChecker::visit(UnaryExpr& node) {
     node.rhs->accept(*this);
 }
 
-void SyntaxTreeChecker::visit(LVal& node) {}
+void SyntaxTreeChecker::visit(LVal& node) {
+    bool is_declared = this->lookup_variable(node.name, this->Expr_int);
+    if (!is_declared){
+        err.error(node.loc, "The variable has NOT been defined.");
+        exit(int(ErrorType::VarUnknown));
+    }
+}
 
 void SyntaxTreeChecker::visit(Literal& node) {
     this->Expr_int = (node.literal_type == SyntaxTree::Type::INT);
@@ -37,6 +46,12 @@ void SyntaxTreeChecker::visit(Literal& node) {
 void SyntaxTreeChecker::visit(ReturnStmt& node) {}
 
 void SyntaxTreeChecker::visit(VarDef& node) {
+    bool is_declared = this->lookup_variable(node.name, this->Expr_int);
+    if (is_declared){
+        err.error(node.loc, "The variable has ALREADY been defined.");
+        exit(int(ErrorType::VarDuplicated));
+    }
+    this->declare_variable(node.name, node.btype);
     if (node.is_inited) {
         node.initializers->accept(*this);
     }
@@ -44,9 +59,13 @@ void SyntaxTreeChecker::visit(VarDef& node) {
 
 void SyntaxTreeChecker::visit(AssignStmt& node) {}
 void SyntaxTreeChecker::visit(FuncCallStmt& node) {}
-void SyntaxTreeChecker::visit(BlockStmt& node) {}
+void SyntaxTreeChecker::visit(BlockStmt& node) {
+    this->enter_scope();
+}
 void SyntaxTreeChecker::visit(EmptyStmt& node) {}
-void SyntaxTreeChecker::visit(SyntaxTree::ExprStmt& node) {}
+void SyntaxTreeChecker::visit(SyntaxTree::ExprStmt& node) {
+    node.exp->accept(*this);
+}
 void SyntaxTreeChecker::visit(SyntaxTree::FuncParam& node) {}
 void SyntaxTreeChecker::visit(SyntaxTree::FuncFParamList& node) {}
 void SyntaxTreeChecker::visit(SyntaxTree::BinaryCondExpr& node) {}
